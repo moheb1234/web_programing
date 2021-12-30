79fbd33242e8830f88f1d095b2dc950383af4e39
moheb1234 <92215781+moheb1234@users.noreply.github.com>
2021-12-30 16:43:08 +0330

    Add files via upload

---
+++ calcPage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>calculator</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <?php include 'calculator.php'; ?>
+
+</head>
+<body>
+<br><br><br>
+<form method="post">
+    <div class="w3-center ">
+        <input name="input" id="input" class="w3-border w3-border-black w3-xlarge"
+               type="text" placeholder="enter a math expression" style="width: 30%">
+        <button class="w3-red w3-xlarge w3-hover-gray w3-border w3-border-black"
+                style="cursor: pointer" name="calc">calculate
+        </button>
+        <p class="w3-xlarge" id="result">  <?php echo calc($_POST['input'], $_POST['calc'], $_POST['clear']); ?> </p>
+        <br>
+        <button class="w3-xlarge w3-blue w3-button " name="clear"
+                style="cursor: pointer"> clear
+        </button>
+    </div>
+</form>
+<br><br>
+
+</body>
+</html>
---
+++ calculator.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL ^ E_WARNING);
+function calc($expression, $calcButton, $clearButton)
+{
+    if (isset($calcButton)) {
+        try {
+            $math = "print" . "(" . $expression . ")" . ";";
+            return eval($math);
+        } catch (Error $e) {
+            return "please enter a valid math expression";
+        }
+    }
+    if (isset($clearButton))
+        return "";
+    return "";
+}
+
+
+
+
